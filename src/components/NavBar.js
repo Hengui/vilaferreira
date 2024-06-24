491b1adad42c94348adc2aa287c33ecdf310b6aa
Guilherme Ferreira <gui_henrique09@hotmail.com>
2024-06-24 10:31:53 -0300

    ideia+sobrenome

---
+++ src/components/NavBar.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+const NavBar = () => {
+  return (
+    <nav id="menu">
+      <a><img id="logo" src="#" alt="Logo da loja"/></a>
+      <div className="navigation">
+        <ul>
+          <li className="list ativo">
+            <a href="#">
+              <span className="icon"><i className="#"></i></span>
+              <span className="text">Equipamentos</span>
+            </a>
+          </li>
+          <li className="list">
+            <a href="#">
+              <span className="icon"><i className="#"></i></span>
+              <span className="text">Ferragens</span>
+            </a>
+          </li>
+          <li className="list">
+            <a href="#">
+              <span className="icon"><i className="#"></i></span>
+              <span className="text">Início</span>
+            </a>
+          </li>
+          <li className="list">
+            <a href="#">
+              <span className="icon"><i className="#"></i></span>
+              <span className="text">Ferramentas</span>
+            </a>
+          </li>
+          <li className="list">
+            <a href="#">
+              <span className="icon"><i className="#"></i></span>
+              <span className="text">Carrinho</span>
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div className="search-container">
+        <input type="text" id="search-input" placeholder="Pesquisar..."/>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
